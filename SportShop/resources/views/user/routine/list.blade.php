ef1f08b18be26d8681caf6d3ae6191fb4d1086fd
Cesar Garcia <cagarciap@eafit.edu.co>
2020-09-15 14:52:50 -0500

    Correciones

--- SportShop/resources/views/user/routine/list.blade.php
+++ SportShop/resources/views/user/routine/list.blade.php
@@ -2,29 +2,42 @@
 
 @section('routine_content')
 <div class="container">
-   <div class="row justify-content-center">
-      <div class="col-md-8">
-         <table class="table table-striped">
-            <thead>
-               <tr>
-                  <th>Id</th>
-                  <th>Name</th>
-                  <th>Show Description</th>
-                  <th colspan="2">Option</th>
-               </tr>
-            </thead>
-            <tbody>
-               @foreach($data["routines"] as $routine)
-               <tr>
-                  <td>{{ $routine->getId() }}</td>
-                  <td>{{ $routine->getName() }}</td>
-                  <td>{{ $routine->getDescription() }}</td>
-                  <td><a class="navbar-brand btn btn-outline-info btn-block" href="{{ route('user.routine.show',['id'=> $routine->getId()])}}"><img src="{{ asset('/icons/file-earmark-text.svg') }}"></a></td>                  
-               </tr>
-               @endforeach
-            </tbody>
-         </table>
-      </div>
-   </div>
+    @if ($data["products"]->isEmpty())
+        <div class="row justify-content-center">
+            <div class="col-md-6">
+                <div class="card">
+                    <div class="card-header">Don't have any products</div>
+                    <div class="card-body">
+                        <a href="{{ route('admin.product.create') }}" class="btn btn-outline-success btn-block">Create Product</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    @else
+        <div class="row justify-content-center">
+            <div class="col-md-8">
+                <table class="table table-striped">
+                    <thead>
+                        <tr>
+                          <th>Id</th>
+                          <th>Name</th>
+                          <th>Show Description</th>
+                          <th colspan="2">Option</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        @foreach($data["routines"] as $routine)
+                        <tr>
+                            <td>{{ $routine->getId() }}</td>
+                            <td>{{ $routine->getName() }}</td>
+                            <td>{{ $routine->getDescription() }}</td>
+                            <td><a class="navbar-brand btn btn-outline-info btn-block" href="{{ route('user.routine.show',['id'=> $routine->getId()])}}"><img src="{{ asset('/icons/file-earmark-text.svg') }}"></a></td>                  
+                        </tr>
+                        @endforeach
+                    </tbody>
+                </table>
+            </div>
+        </div>
+    @endif
 </div>
 @endsection
