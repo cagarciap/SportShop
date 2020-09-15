494ab1bdb2c68287b7192e02021e36b63b3a5958
Cesar Garcia <cagarciap@eafit.edu.co>
2020-09-15 15:52:00 -0500

    Correciones

--- SportShop/resources/views/layouts/master.blade.php
+++ SportShop/resources/views/layouts/master.blade.php
@@ -12,7 +12,7 @@
         <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
             <div class="container">
                 <a class="navbar-brand" href="{{ url('/home') }}">
-                    <img src="{{ asset('/img/logo.jpeg') }}" class="delete-icon">
+                    <img src="{{ asset('/img/logo.jpeg') }}" class="delete-icon" style="height: 200px; width: 200px">
                     SportShop
                 </a>
                 <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
